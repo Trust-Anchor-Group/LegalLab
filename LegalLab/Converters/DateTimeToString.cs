eae286a7e94b8d0d18b1bba65a86ce5999c3ae29
Peter Waher <peterwaher@hotmail.com>
2021-12-04 22:03:03 -0300

    Wallet

--- LegalLab/Converters/DateTimeToString.cs
+++ LegalLab/Converters/DateTimeToString.cs
@@ -1,20 +1,34 @@
 ﻿using System;
 using System.Globalization;
-using System.Windows;
 using System.Windows.Data;
 using System.Windows.Markup;
 
 namespace LegalLab.Converters
 {
 	/// <summary>
-	/// Converts values to strings.
+	/// Converts <see cref="DateTime"/> values to strings.
 	/// </summary>
-	public class ToString : MarkupExtension, IValueConverter
+	public class DateTimeToString : MarkupExtension, IValueConverter
 	{
 		/// <inheritdoc/>
 		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
 		{
-			return value.ToString();
+			if (value is DateTime TP)
+			{
+				if (TP == DateTime.MinValue || TP == DateTime.MaxValue)
+					return string.Empty;
+				else
+				{
+					string s = TP.ToShortDateString() + ", " + TP.ToLongTimeString();
+
+					if (TP.Kind == DateTimeKind.Utc)
+						s += "Z";
+
+					return s;
+				}
+			}
+			else
+				return value.ToString();
 		}
 
 		/// <inheritdoc/>
--- LegalLab/Models/Wallet/WalletModel.cs
+++ LegalLab/Models/Wallet/WalletModel.cs
@@ -1,5 +1,6 @@
 ﻿using EDaler;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Waher.Networking.XMPP;
 using Waher.Networking.XMPP.Contracts;
@@ -13,7 +14,14 @@ namespace LegalLab.Models.Wallet
 	[Singleton]
 	public class WalletModel : PersistedModel, IDisposable
 	{
+		private Property<double> amount;
+		private Property<string> currency;
+		private Property<DateTime> timestamp;
+
+		private readonly List<AccountEvent> events = new List<AccountEvent>();
+
 		private readonly EDalerClient eDalerClient;
+		private Balance balance = null;
 
 		/// <summary>
 		/// Wallet Model
@@ -24,13 +32,90 @@ namespace LegalLab.Models.Wallet
 		public WalletModel(XmppClient Client, ContractsClient Contracts, string ComponentJid)
 			: base()
 		{
+			this.amount = new Property<double>(nameof(this.Amount), 0, this);
+			this.currency = new Property<string>(nameof(this.Currency), string.Empty, this);
+			this.timestamp = new Property<DateTime>(nameof(this.Timestamp), DateTime.MinValue, this);
+
 			this.eDalerClient = new EDalerClient(Client, Contracts, ComponentJid);
+			this.eDalerClient.BalanceUpdated += EDalerClient_BalanceUpdated;
+		}
+
+		private Task EDalerClient_BalanceUpdated(object Sender, BalanceEventArgs e)
+		{
+			this.Balance = e.Balance;
+
+			lock (this.events)
+			{
+				this.events.Add(e.Balance.Event);
+
+			}
+
+			this.RaisePropertyChanged(nameof(this.Events));
+
+			return Task.CompletedTask;
 		}
 
 		/// <inheritdoc/>
 		public void Dispose()
 		{
 			this.eDalerClient.Dispose();
+		}
+
+		/// <summary>
+		/// Balance amount
+		/// </summary>
+		public double Amount
+		{
+			get => this.amount.Value;
+			set => this.amount.Value = value;
+		}
+
+		/// <summary>
+		/// Balance currency
+		/// </summary>
+		public string Currency
+		{
+			get => this.currency.Value;
+			set => this.currency.Value = value;
+		}
+
+		/// <summary>
+		/// Balance timestamp
+		/// </summary>
+		public DateTime Timestamp
+		{
+			get => this.timestamp.Value;
+			set => this.timestamp.Value = value;
+		}
+
+		/// <summary>
+		/// Latest balance
+		/// </summary>
+		public Balance Balance
+		{
+			get => this.balance;
+			set
+			{
+				this.balance = value;
+
+				this.Amount = (double)this.balance.Amount;
+				this.Currency = this.balance.Currency;
+				this.Timestamp = this.balance.Timestamp;
+			}
+		}
+
+		/// <summary>
+		/// Account events
+		/// </summary>
+		public IEnumerable<AccountEvent> Events
+		{
+			get
+			{
+				lock (this.events)
+				{
+					return this.events.ToArray();
+				}
+			}
 		}
 
 		/// <inheritdoc/>
@@ -40,6 +125,19 @@ namespace LegalLab.Models.Wallet
 			{
 				MainWindow.currentInstance.WalletTab.DataContext = this;
 			});
+
+			this.Balance = await this.eDalerClient.GetBalanceAsync();
+
+			(AccountEvent[] Events, bool More) = await this.eDalerClient.GetAccountEventsAsync(50);
+
+			lock (this.events)
+			{
+				this.events.AddRange(Events);
+			}
+
+			this.RaisePropertyChanged(nameof(this.Events));
+
+			(decimal Amount, string Currency, PendingPayment[] Pending) = await this.eDalerClient.GetPendingPayments();
 
 			await base.Start();
 		}
