1b53be600cb882c64517be84313a0456c3315040
Peter Waher <peterwaher@hotmail.com>
2021-12-14 20:56:23 -0300

    Putting Legal ID tab in separate user control

---
+++ LegalLab/Tabs/LegalIdTab.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Controls;
+
+namespace LegalLab.Tabs
+{
+	/// <summary>
+	/// Interaction logic for LegalIdTab.xaml
+	/// </summary>
+	public partial class LegalIdTab : UserControl
+	{
+		public LegalIdTab()
+		{
+			InitializeComponent();
+		}
+	}
+}
