272ab5a5fff3c0b93b2e9939e946c0c624796c49
Peter Waher <peterwaher@hotmail.com>
2021-12-14 21:34:57 -0300

    Putting Design tab in separate user control

---
+++ LegalLab/Tabs/DesignTab.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Controls;
+
+namespace LegalLab.Tabs
+{
+	/// <summary>
+	/// Interaction logic for DesignTab.xaml
+	/// </summary>
+	public partial class DesignTab : UserControl
+	{
+		public DesignTab()
+		{
+			InitializeComponent();
+		}
+	}
+}
