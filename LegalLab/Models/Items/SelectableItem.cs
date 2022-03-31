1b0d0a35db27a485c0041b92a13737cfb77019fc
Peter Waher <peterwaher@hotmail.com>
2022-03-31 12:02:16 -0300

    Fix

--- LegalLab/Models/Items/SelectableItem.cs
+++ LegalLab/Models/Items/SelectableItem.cs
@@ -44,6 +44,7 @@ namespace LegalLab.Models.Items
 				if (this.selected != value)
 				{
 					this.selected = value;
+					this.RaisePropertyChanged(nameof(IsSelected));
 
 					if (this.selected)
 						this.OnSelected();
